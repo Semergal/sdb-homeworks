1fe2fb910138dc13cfdb59f6aea0fcbc4f16a4ee
Konstantin <troshinkv@mail.ru>
2024-07-18 15:25:30 +1000

    3

--- 11-04/consumer.py
+++ 11-04/consumer.py
@@ -1,15 +1,29 @@
-#!/usr/bin/env python
-# coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.queue_declare(queue='hello')
+username = 'test'
+password = 'test'
 
+credentials = pika.PlainCredentials(username, password)
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=credentials))
+channel = connection.channel()
+
+
+try:
+    queue_declare_ok = channel.queue_declare(queue='hello', passive=True)
+except pika.exceptions.ChannelClosedByBroker as e:
+    error_code, error_text = e.args
+    if error_code == 406:
+        print(f"Очередь 'hello' уже существует с другими параметрами: {error_text}")
+        
+        queue_declare_ok = channel.queue_declare(queue='hello', durable=True)
+    else:
+        print(f"Ошибка при объявлении очереди: {error_code} - {error_text}")
+        raise e
+else:
+    queue_name = queue_declare_ok.method.queue
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
-
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
--- 11-04/producer.py
+++ 11-04/producer.py
@@ -1,9 +1,28 @@
-#!/usr/bin/env python
-# coding=utf-8
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+username = 'test'
+password = 'test'
+
+credentials = pika.PlainCredentials(username, password)
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=credentials))
 channel = connection.channel()
-channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='', routing_key='hello', body='Hello Netology!')
+
+try:
+    channel.queue_declare(queue='hello', passive=True)
+except pika.exceptions.ChannelClosedByBroker as e:
+    error_code, error_text = e.args
+    if error_code == 406:
+        print(f"Очередь 'hello' уже существует с другими параметрами: {error_text}")
+        
+        queue_declare_ok = channel.queue_declare(queue='hello', durable=True)
+        queue_name = queue_declare_ok.method.queue
+    else:
+        print(f"Ошибка при объявлении очереди: {error_code} - {error_text}")
+        raise e
+else:
+    queue_name = 'hello'
+
+
+channel.basic_publish(exchange='', routing_key=queue_name, body='Hello Netology!')
+
 connection.close()
